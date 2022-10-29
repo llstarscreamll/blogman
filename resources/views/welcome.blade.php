b198a6d977c35e0e83ec4b9ffd04a3675e36292d
Johan Alvarez <llstarscreamll@hotmail.com>
2022-10-29 17:42:15 -0500

    feat: change default Laravel root page links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,12 +83,9 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/llstarscreamll">Johan at GitHub</a>
+                    <a href="https://www.linkedin.com/in/johan-alvarez/">Johan at LinkedIn</a>
+                    <a href="https://twitter.com/J0H4N_CO">Johan at Twitter</a>
                 </div>
             </div>
         </div>
