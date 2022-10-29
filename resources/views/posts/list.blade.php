071db15342b5a36255b496b3bb57d8834283dcae
Johan Alvarez <llstarscreamll@hotmail.com>
2022-10-29 18:54:48 -0500

    refactor(posts): improve some visuals on posts list page

--- resources/views/posts/list.blade.php
+++ resources/views/posts/list.blade.php
@@ -32,7 +32,7 @@
                                         <td>
                                             <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
                                                 @method('DELETE') @csrf
-                                                <button type="submit">Delete</button>
+                                                <button type="submit" class="btn">Delete</button>
                                             </form>
                                         </td>
                                     </tr>
